92126aafa1f80fd7f4dca057e85a52fe4a71175b
Akil Lutes <lutes.akil@gmail.com>
2021-07-28 13:25:51 -0500

    refactored app.js to main.js

--- src/components/DirectoryComponent.js
+++ src/components/DirectoryComponent.js
@@ -1,28 +1,13 @@
 import React, { Component } from 'react'
 import { Card, CardImg, CardImgOverlay, CardTitle } from 'reactstrap';
-import CampsiteInfo from './CampsiteInfo'
-
-// campsites is a prop, campsite is mapped
-// I made selectedCampsite state null so a card wouldn't be selected yet.
-// then made a method to where I setState to selectedCampsite prop.
 
 class Directory extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            selectedCampsite: null
-        };
-    }
-
-    onCampsiteSelect(campsite) {
-        this.setState({selectedCampsite: campsite});
-    }
 
     render() {
         const directory = this.props.campsites.map(campsite => {
             return (
                 <div key={campsite.id} className="col-md-5 m-1">
-                    <Card onClick={() => this.onCampsiteSelect(campsite)}>
+                    <Card onClick={() => this.props.onClick(campsite.id)}>
                         <CardImg width="100%" src={campsite.image} alt={campsite.name} />
                         <CardImgOverlay>
                             <CardTitle>{campsite.name}</CardTitle>
@@ -36,7 +21,6 @@ class Directory extends Component {
             <div className="container">
                 <div className="row">
                     {directory}
-                    <CampsiteInfo campsite={this.state.selectedCampsite} comments={this.props.comments} />
                 </div>
                 
             </div>
