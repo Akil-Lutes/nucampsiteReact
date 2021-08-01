a4fde9d576096b9b84f37145d39e5f73fe4a5899
Akil Lutes <lutes.akil@gmail.com>
2021-07-31 23:57:34 -0500

    made react router changes

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import Directory from './DirectoryComponent';
-import { CAMPSITES } from '../shared/campsites';
 import Header from './HeaderComponent'
 import Footer from './FooterComponent'
 import CampsiteInfo from './CampsiteInfo'
 import Home from './HomeComponent';
+import About from './AboutComponent'
+import Contact from './ContactComponent'
 import { Switch, Route, Redirect } from 'react-router-dom';
+import { CAMPSITES } from '../shared/campsites';
+import { COMMENTS } from '../shared/comments';
+import { PARTNERS } from '../shared/partners';
+import { PROMOTIONS } from '../shared/promotions';
 
 // setting up the brains of react-router, to know where to send users when links are clicked
 
@@ -19,14 +24,38 @@ class Main extends Component {
         super(props);
         this.state = {
             campsites: CAMPSITES,
+            comments: COMMENTS,
+            partners: PARTNERS,
+            promotions: PROMOTIONS
         };
     }
 
     render() {
-
+        // Used arrow function bc this keyword inherits from parent scope. Function declartion would not point to state of parent class (undefined)
         const HomePage = () => {
             return (
-                <Home />
+                <Home 
+                    campsite={this.state.campsites.filter(campsite => campsite.featured)[0]}
+                    promotion={this.state.promotions.filter(promotion => promotion.featured)[0]}
+                    partner={this.state.partners.filter(partner => partner.featured)[0]}
+                />
+            );
+        };
+
+        const AboutPage = () => {
+            return (
+                <About
+                    partners={this.state.partners}
+                />
+            )
+        }
+
+        const CampsiteWithId = ({match}) => {
+            return (
+                <CampsiteInfo 
+                    campsite={this.state.campsites.filter(campsite => campsite.id === +match.params.campsiteId)[0]}
+                    comments={this.state.comments.filter(comment => comment.campsiteId === +match.params.campsiteId)}
+                />
             );
         };
 
@@ -36,8 +65,10 @@ class Main extends Component {
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/directory' render={() => <Directory campsites={this.state.campsites} />} />
+                    <Route path='/directory/:campsiteId' component={CampsiteWithId} />
+                    <Route exact path='/contactus' component={Contact} />
+                    <Route path='/aboutus' component={AboutPage} />
                     <Redirect to='/home' />
-                    <Directory campsites={this.state.campsites} />
                 </Switch>
                 <Footer />
             </div>
@@ -46,3 +77,6 @@ class Main extends Component {
 }
 
 export default Main;
+
+
+// <Directory campsites={this.state.campsites} />
